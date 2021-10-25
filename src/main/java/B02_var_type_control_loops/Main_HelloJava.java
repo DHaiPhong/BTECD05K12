25c106414a5efff13a9fbd360df768acc82bb1f2
DHaiPhong <92972634+DHaiPhong@users.noreply.github.com>
2021-10-25 13:33:33 +0700

    Initial commit

---
+++ src/main/java/B02_var_type_control_loops/Main_HelloJava.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B02_var_type_control_loops;
+
+/**
+ *
+ * @author NC
+ */
+public class Main_HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Sekai");
+    }
+    
+}
